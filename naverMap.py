ae474d19eed79aef5a00974d1a2b66785fdb82d4
Youngmin Park <angksflvlf52@gmail.com>
2022-03-23 15:22:53 +0900

    Update naverMap.py

--- naverMap.py
+++ naverMap.py
@@ -1,14 +1,4 @@
 import streamlit as st
-import xml.etree.ElementTree as ET
-import requests
-import pymysql
-from datetime import datetime
-import json
-import requests
-import pandas as pd
-from urllib import parse
-import sys
-from st_aggrid import AgGrid
 
 def naverMap():
     for i in range(2021,2031):
